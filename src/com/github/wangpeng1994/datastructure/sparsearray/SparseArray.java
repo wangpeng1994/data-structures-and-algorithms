a80acae0af08d88edd8649fc54e68db008b04d18
wangpeng1994 <947034046@qq.com>
2019-10-20 15:24:43 +0800

    implement the sparse array

--- src/com/github/wangpeng1994/datastructure/sparsearray/SparseArray.java
+++ src/com/github/wangpeng1994/datastructure/sparsearray/SparseArray.java
@@ -3,59 +3,93 @@ package com.github.wangpeng1994.datastructure.sparsearray;
 public class SparseArray {
 
     public static void main(String[] args) {
-        // Create a original two-dimensional array sample.
-        // 0: no chess piece  1: black piece  2: blue piece
-        int[][] chessArr1 = new int[11][11];
-        chessArr1[1][2] = 1;
-        chessArr1[2][3] = 2;
+        // 创建一个原始二维数组，如 11x11
+        int[][] twoDArr = new int[11][11];
+        twoDArr[1][2] = 1;
+        twoDArr[2][3] = 2;
+        twoDArr[3][0] = 1;
 
-        for (int[] row : chessArr1) {
+        for (int[] row : twoDArr) {
             for (int data : row) {
                 System.out.printf("%d\t", data);
             }
             System.out.println();
         }
 
-        // Transform two-dimensional array into sparse array.
+        int[][] sparseArr = twoDArr2SparseArr(twoDArr);
 
-        // 1. Loop through the two-dimensional array to get the count of nonzero data.
+        SparseArr2TwoDArr(sparseArr);
+    }
+
+    /**
+     * 二维数组 转 稀疏数组
+     *
+     * @param twoDArr 原始二维数组
+     * @return 转换后的稀疏数组
+     */
+    private static int[][] twoDArr2SparseArr(int[][] twoDArr) {
+        int rows = twoDArr.length;
+        int cols = twoDArr[0].length;
+
+        // 1. 遍历二维数组得到非零数据的总数
         int sum = 0;
-        for (int i = 0; i < chessArr1.length; i++) {
-            for (int j = 0; j < chessArr1[0].length; j++) {
-                if (chessArr1[i][j] != 0) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (twoDArr[i][j] != 0) {
                     sum++;
                 }
             }
         }
 
-        // 2. Create the corresponding sparse array.
+        // 2. 创建相应稀疏数组并为第一行赋值
         int[][] sparseArr = new int[sum + 1][3];
-        // Assign values to this array.
-        sparseArr[0][0] = 11;
-        sparseArr[0][1] = 11;
+        sparseArr[0][0] = rows;
+        sparseArr[0][1] = cols;
         sparseArr[0][2] = sum;
 
-        // 3. Loop through the two-dimensional array again to assign nonzero data into the sparse array.
-        int count = 0; // for record how many times met nonzero data
-        for (int i = 0; i < chessArr1.length; i++) {
-            for (int j = 0; j < chessArr1[0].length; j++) {
-                if (chessArr1[i][j] != 0) {
+        // 3. 再次遍历二维数组将有效数据填充到稀疏数组中
+        int count = 0; // 用于记录有效数据在稀疏数组中应处的行数
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (twoDArr[i][j] != 0) {
                     count++;
                     sparseArr[count][0] = i;
                     sparseArr[count][1] = j;
-                    sparseArr[count][2] = chessArr1[i][j];
+                    sparseArr[count][2] = twoDArr[i][j];
                 }
             }
         }
 
+        // 输出稀疏数组
         for (int[] row : sparseArr) {
+            System.out.printf("%d\t%d\t%d\n", row[0], row[1], row[2]);
+        }
+
+        return sparseArr;
+    }
+
+    /**
+     * 稀疏数组 恢复到 原始二维数组
+     *
+     * @param sparseArr 稀疏数组
+     * @return 原始二维数组
+     */
+    private static int[][] SparseArr2TwoDArr(int[][] sparseArr) {
+        // 1. 读取稀疏数组第一行的数据，创建原始二维数组
+        int[][] twoDArr = new int[sparseArr[0][0]][sparseArr[0][1]];
+
+        // 2. 再读取稀疏数组其余行的数据，恢复原始二维数组
+        for (int i = 1; i < sparseArr.length; i++) {
+            twoDArr[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+
+        // 输出恢复后的二维数组
+        for (int[] row : twoDArr) {
             for (int data : row) {
                 System.out.printf("%d\t", data);
             }
             System.out.println();
         }
-
-
+        return twoDArr;
     }
-
 }
